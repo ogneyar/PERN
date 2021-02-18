e7e0e8cb7a89218102db4fa3174b94ed26b3a4ae
ogneyar <ogneyar-ne@ya.ru>
2021-02-18 21:01:06 +0300

    test

--- server/db.js
+++ server/db.js
@@ -6,7 +6,7 @@ module.exports = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASS,
     {
-        dialect: 'mysql',
+        dialect: process.env.DB,
         host: process.env.DB_HOST,
         port: process.env.DB_PORT 
     }
