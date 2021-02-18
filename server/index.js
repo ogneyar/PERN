0bf2cde21f818b9186dd2cb1465d197f28ff5d05
ogneyar <ogneyar-ne@ya.ru>
2021-02-18 13:13:36 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+app.get('/', (req, res) => {
+    // console.log("log");
+    res.send("PERN - приветствует тебя!")
+
+})
+
+app.listen(PORT, () => console.log(`Server run: http://localhost:${PORT}`))
