55e54f60fde4dd58979430b58273e4e8fe05eef0
ogneyar <ogneyar-ne@ya.ru>
2021-02-24 12:25:38 +0300

    eee

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,20 +1,33 @@
 import React, { useContext } from 'react'
 import { Context } from '..'
 import { Navbar, Nav, Button, Container } from 'react-bootstrap'
-import { NavLink } from 'react-router-dom'
-import { SHOP_ROUTE } from '../utils/consts'
+import { NavLink, useHistory } from 'react-router-dom'
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from '../utils/consts'
 import { observer } from 'mobx-react-lite'
+
 
 const NavBar = observer(() => {
     const {user} = useContext(Context)
+    const history = useHistory()
     return (
         <Navbar bg="dark" variant="dark" className="NavBar">
             <Container>
                 <NavLink style={{color: 'white'}} to={SHOP_ROUTE}>КупиДевайс</NavLink>
                 {user.isAuth ?
                     <Nav className="ml-auto">
-                        <Button variant={'outline-light'}>Админ панель</Button>
-                        <Button className="ml-2" onClick={() => user.setIsAuth(false)} variant={'outline-light'}>Выйти</Button>
+                        <Button 
+                            variant={'outline-light'} 
+                            onClick={() => history.push(ADMIN_ROUTE)}
+                        >
+                            Админ панель
+                        </Button>
+                        <Button 
+                            variant={'outline-light'} 
+                            onClick={() => history.push(LOGIN_ROUTE)} 
+                            className="ml-2"
+                        >
+                            Выйти
+                        </Button>
                     </Nav>
                     :
                     <Nav className="ml-auto">                    
